cf78c2f8f046bcf2aa77983c2de37dcbb107c153
Grace Carola T <gracetorrico1@upb.edu>
2023-04-18 10:01:27 -0400

    Swagger instaled and configured for dinamic title

--- PracticeTwo/Program.cs
+++ PracticeTwo/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,21 +6,37 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+var configurationBuilder = new ConfigurationBuilder()
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", optional:false, reloadOnChange:true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional:false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+IConfiguration Configuration = configurationBuilder.Build(); 
+string siteTitle = Configuration.GetSection("Title").Value;
+
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = siteTitle
+    });
+});
+
+//Levantarlo
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Configure the HTTP request pipeline. (Configuración)
+app.UseSwagger();
+app.UseSwaggerUI();
+/*if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    
+}*/
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
